ab831deea8de9079e338f476f5e8a3fc182cfd44
Eliza Upadhyaya <eliza.upadhyaya@gmail.com>
2023-02-17 16:01:42 +0000

    set up routes, set up controller for items, set up view for homepage and new page

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,50 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :destroy, :edit, :update]
+
+  # def index
+  #   @items = Item.all
+  # end
+
+  def show
+    # @task = Task.find(params[:id]) -> refactored and added to set_item
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to item_path(@item)
+  end
+
+  def destroy
+    # @task = Task.find(params[:id]) -> refactored and added to set_item
+    @item.destroy
+    redirect_to items_path # status: :see_other
+  end
+
+  def edit
+    # @item = Item.find(params[:id]) -> refactored and added to set_item
+  end
+
+  def update
+    # @item = Item.find(params[:id]) -> refactored and added to set_item
+    # raise
+    @item.update(item_params)
+    edirect_to item_path(@item)
+    # else
+    # render "edit" #will go back / stay on the same page
+  end
+
+  private
+
+  def item_parms
+    params.require(:item).permit(:name, :price, :condition, :category, :brand, :available)
+  end
+
+  def set_item
+    @item = item.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,31 @@
 Rails.application.routes.draw do
+  get 'items/new'
   devise_for :users
+  # Homepage where you can see all clothes
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Show / Items
+  get "items/:id", to: "items#show", as: :item
+
+  # New / Items
+  get "items/new", to: "items#new", as: :new_item
+  post "items", to: "items#create"
+
+  # Edit / Items
+  get "items/:id/edit", to: "items#show", as: :edit_item
+  patch "items/:id", to: "items#update"
+
+  # Destroy / Items
+  delete "items/:id", to: "items#destroy"
+
+  # Profile
+  # get "/profile", to: "profile#show"
+
+  # Rental "checkout"
+  # get "/items/rental/new", to: "rental#new"
+  # post "items/rental", to: "rental#create"
 end
