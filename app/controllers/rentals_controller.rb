0d7c46f630d2f3f2e2a307420e377d2b76467233
Mary Traynor <maryotraynor@gmail.com>
2023-02-24 16:47:34 +0100

    rentals_controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,5 +1,24 @@
 class RentalsController < ApplicationController
-end
+  # before_action :authenticate_user!
 
-def create
+  def show
+  end
+
+  def create
+    @rental = Rental.new(rental_params)
+    @rental.item = Item.find(params[:item_id])
+  end
+
+
+  def rental_request
+  end
+
+  def confirmation
+    if
+  end
+
+
+  def rental_params
+    params.require(:rental).permit(:start_date, :end_date, :confirmed, :user_id, :item_id)
+  end
 end
